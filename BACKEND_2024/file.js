3f5ab70b3608b36595411e6008d95ab61960cdca
Chaudary Hammad <102407219+ChaudaryHammad@users.noreply.github.com>
2024-01-16 23:09:36 +0500

    starting with expressjs

--- BACKEND_2024/file.js
+++ BACKEND_2024/file.js
@@ -1,5 +1,37 @@
 const fs = require('fs')
 
 // //synchronus call
-// fs.writeFileSync('./test.txt',"hi, hammad")
-fs.writeFile('./test.txt',"hi, hammad", (err)=>{console.log(err);})
+ fs.writeFileSync('./test.txt',"hi, hammad")
+
+//Asyschronus call
+fs.writeFile('./test.txt',"hi, hammad bai", (err)=>{console.log(err);})
+
+
+//reading a file
+fs.readFile('./contacts.txt',"utf-8",(err,data)=>{
+if(err){
+    console.log(err);
+}else{
+    console.log(data);
+}
+})
+
+
+
+// synch reading can return the data in a varibale nut async cant
+
+// append data in a file
+
+fs.appendFileSync('./contacts.txt',`\n ${Date.now()} hi`
+   )
+
+
+//copy 1 file to other 
+fs.cpSync('./contacts.txt','./test.txt')
+
+
+//delete file
+fs.unlinkSync('./test.txt')
+
+
+console.log(fs.statSync('./contacts.txt').isFile());
--- BACKEND_2024/server.js
+++ BACKEND_2024/server.js
@@ -1,11 +1,45 @@
-const express = require('express')
+const express = require("express");
 const PORT = 5000;
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send("Hello World");
-})
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
-})
+// app.get("/", (req, res) => {
+//   const log = `${Date.now()}: ${req.url} New request Recieved \n`;
+//   const myurl = url.parse(req.url,true);
+//   console.log(myurl);
+//   fs.appendFile("log.txt", log, (err, data) => {
+//     switch (req.url) {
+//       case "/":
+//         res.end("home page");
+//         break;
+//       case "/about":
+//         res.end("about page");
+//         break;
+//       case "/contact":
+//         res.end("contact page");
+//         break;
+//     }clear
+//   });
+// });
+
+
+
+app.get("/", (req, res) => {
+ return  res.send("home page");
+});
+
+app.get("/about", (req, res) => {
+  return res.send("about page" + " hi " + req.query.name);
+});
+
+app.get("/contact", (req, res) => {
+  return res.send("contact page");
+});
+
+
+
+
+// listening to server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
